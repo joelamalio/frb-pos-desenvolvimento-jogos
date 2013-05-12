a030d37790237d4a5e8bd6030b9acf48c8806293
Joel Amálio <joel.amalio@gmail.com>
2013-05-12 11:52:05 -0300

    Atualização das bandeiras.

--- js/game.js
+++ js/game.js
@@ -5,33 +5,33 @@ var player = new Player(context);
 var interval;
 
 game_manager.set_player(player);
-game_manager.add_enemy("images/flag_germany.bmp", 0, 40);
-game_manager.add_enemy("images/flag_argentina.bmp", 80, 40);
-game_manager.add_enemy("images/flag_argentina.bmp", 160, 40);
-game_manager.add_enemy("images/flag_argentina.bmp", 240, 40);
-game_manager.add_enemy("images/flag_argentina.bmp", 320, 40);
-game_manager.add_enemy("images/flag_argentina.bmp", 400, 40);
+game_manager.add_enemy("images/flag/afs.bmp", 0, 40);
+game_manager.add_enemy("images/flag/ang.bmp", 80, 40);
+game_manager.add_enemy("images/flag/argentina.bmp", 160, 40);
+game_manager.add_enemy("images/flag/bel.bmp", 240, 40);
+game_manager.add_enemy("images/flag/bul.bmp", 320, 40);
+game_manager.add_enemy("images/flag/cam.bmp", 400, 40);
 
-game_manager.add_enemy("images/flag_portugal.bmp", 0 , 100);
-game_manager.add_enemy("images/flag_portugal.bmp", 80 , 100);
-game_manager.add_enemy("images/flag_argentina.bmp", 160, 100);
-game_manager.add_enemy("images/flag_argentina.bmp", 240, 100);
-game_manager.add_enemy("images/flag_argentina.bmp", 320, 100);
-game_manager.add_enemy("images/flag_argentina.bmp", 400, 100);
+game_manager.add_enemy("images/flag/can.bmp", 0 , 100);
+game_manager.add_enemy("images/flag/chl.bmp", 80 , 100);
+game_manager.add_enemy("images/flag/cub.bmp", 160, 100);
+game_manager.add_enemy("images/flag/din.bmp", 240, 100);
+game_manager.add_enemy("images/flag/esp.bmp", 320, 100);
+game_manager.add_enemy("images/flag/eua.bmp", 400, 100);
 
-game_manager.add_enemy("images/flag_portugal.bmp", 0 , 160);
-game_manager.add_enemy("images/flag_portugal.bmp", 80 , 160);
-game_manager.add_enemy("images/flag_argentina.bmp", 160, 160);
-game_manager.add_enemy("images/flag_argentina.bmp", 240, 160);
-game_manager.add_enemy("images/flag_argentina.bmp", 320, 160);
-game_manager.add_enemy("images/flag_argentina.bmp", 400, 160);
+game_manager.add_enemy("images/flag/germany.bmp", 0 , 160);
+game_manager.add_enemy("images/flag/gre.bmp", 80 , 160);
+game_manager.add_enemy("images/flag/jap.bmp", 160, 160);
+game_manager.add_enemy("images/flag/mex.bmp", 240, 160);
+game_manager.add_enemy("images/flag/nig.bmp", 320, 160);
+game_manager.add_enemy("images/flag/nor.bmp", 400, 160);
 
-game_manager.add_enemy("images/flag_portugal.bmp", 0 , 220);
-game_manager.add_enemy("images/flag_portugal.bmp", 80 , 220);
-game_manager.add_enemy("images/flag_argentina.bmp", 160, 220);
-game_manager.add_enemy("images/flag_argentina.bmp", 240, 220);
-game_manager.add_enemy("images/flag_argentina.bmp", 320, 220);
-game_manager.add_enemy("images/flag_argentina.bmp", 400, 220);
+game_manager.add_enemy("images/flag/par.bmp", 0 , 220);
+game_manager.add_enemy("images/flag/portugal.bmp", 80 , 220);
+game_manager.add_enemy("images/flag/rus.bmp", 160, 220);
+game_manager.add_enemy("images/flag/sen.bmp", 240, 220);
+game_manager.add_enemy("images/flag/tur.bmp", 320, 220);
+game_manager.add_enemy("images/flag/uru.bmp", 400, 220);
 
 function loop_game() {
   if (game_manager.started && !game_manager.paused && !game_manager.lost && !game_manager.won) {
--- js/player.js
+++ js/player.js
@@ -2,7 +2,7 @@ var Player = Entity.extend({
 
   init: function(context) {
     this.context = context;
-    this.set_image("images/flag_brazil.bmp");
+    this.set_image("images/flag/brazil.bmp");
     this.set_position({x: 380, y: 570});
     this.set_size({width: 39, height: 29});
     this.set_velocity(5);
